cf9df84cdde716700567f9c07a22a4bf4a0026c5
Nikolaj787 <carlsoncooldk@gmail.com>
2021-11-08 12:24:28 +0100

    Instant grayscale on Form2

--- Webcam_AForge_Edition/Form2.cs
+++ Webcam_AForge_Edition/Form2.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AForge.Imaging.Filters;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -17,9 +18,24 @@ namespace Webcam_AForge_Edition
 
         public Form2(Image im)
         {
+
             Imagefromform1 = im;
+
             InitializeComponent();
-            pictureBoxForm2.Image = Imagefromform1;
+
+            pictureBoxForm2.Image = Imagefromform1; //Displays the picture in the picturebox
+
+            //Creation of greyscale filter
+            Grayscale filter = new Grayscale(0.2125, 0.7154, 0.0721);
+            Bitmap bt = new Bitmap(pictureBoxForm2.Image);
+
+            //applying the filter
+            Bitmap grayImage = filter.Apply(bt);
+            pictureBoxForm2.Image = (System.Drawing.Image)grayImage.Clone(); //clones the picture grayImage and displays it on the left
+
+            
+            
+
 
 
         }
