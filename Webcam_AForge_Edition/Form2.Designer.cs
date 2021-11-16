781c506c853bac192e443c6fb3468c767d863ff4
niko391a <69769473+niko391a@users.noreply.github.com>
2021-11-16 08:54:31 +0100

    Trackbar added

--- Webcam_AForge_Edition/Form2.Designer.cs
+++ Webcam_AForge_Edition/Form2.Designer.cs
@@ -28,56 +28,73 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
             this.pictureBoxForm2 = new System.Windows.Forms.PictureBox();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxForm2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
             this.SuspendLayout();
             // 
             // chart1
             // 
-            chartArea1.AxisX.MajorGrid.Enabled = false;
-            chartArea1.AxisX.Maximum = 255D;
-            chartArea1.AxisX.Minimum = 0D;
-            chartArea1.AxisY.Enabled = System.Windows.Forms.DataVisualization.Charting.AxisEnabled.False;
-            chartArea1.Name = "ChartArea1";
-            this.chart1.ChartAreas.Add(chartArea1);
-            legend1.Enabled = false;
-            legend1.Name = "Legend1";
-            this.chart1.Legends.Add(legend1);
-            this.chart1.Location = new System.Drawing.Point(388, 67);
+            chartArea2.AxisX.MajorGrid.Enabled = false;
+            chartArea2.AxisX.Maximum = 255D;
+            chartArea2.AxisX.Minimum = 0D;
+            chartArea2.AxisY.Enabled = System.Windows.Forms.DataVisualization.Charting.AxisEnabled.False;
+            chartArea2.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea2);
+            legend2.Enabled = false;
+            legend2.Name = "Legend1";
+            this.chart1.Legends.Add(legend2);
+            this.chart1.Location = new System.Drawing.Point(776, 129);
+            this.chart1.Margin = new System.Windows.Forms.Padding(6);
             this.chart1.Name = "chart1";
-            series1.ChartArea = "ChartArea1";
-            series1.Legend = "Legend1";
-            series1.Name = "Series1";
-            this.chart1.Series.Add(series1);
-            this.chart1.Size = new System.Drawing.Size(383, 329);
+            series2.ChartArea = "ChartArea1";
+            series2.Legend = "Legend1";
+            series2.Name = "Series1";
+            this.chart1.Series.Add(series2);
+            this.chart1.Size = new System.Drawing.Size(766, 633);
             this.chart1.TabIndex = 0;
             this.chart1.Text = "chart1";
             // 
             // pictureBoxForm2
             // 
-            this.pictureBoxForm2.Location = new System.Drawing.Point(12, 67);
+            this.pictureBoxForm2.Location = new System.Drawing.Point(24, 129);
+            this.pictureBoxForm2.Margin = new System.Windows.Forms.Padding(6);
             this.pictureBoxForm2.Name = "pictureBoxForm2";
-            this.pictureBoxForm2.Size = new System.Drawing.Size(334, 329);
+            this.pictureBoxForm2.Size = new System.Drawing.Size(668, 633);
             this.pictureBoxForm2.TabIndex = 1;
             this.pictureBoxForm2.TabStop = false;
             // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(869, 771);
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(584, 90);
+            this.trackBar1.TabIndex = 2;
+            // 
             // Form2
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(1600, 865);
+            this.Controls.Add(this.trackBar1);
             this.Controls.Add(this.pictureBoxForm2);
             this.Controls.Add(this.chart1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(6);
             this.Name = "Form2";
             this.Text = "Form2";
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxForm2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -85,5 +102,6 @@
 
         private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
         private System.Windows.Forms.PictureBox pictureBoxForm2;
+        private System.Windows.Forms.TrackBar trackBar1;
     }
 }
--- Webcam_AForge_Edition/Form2.cs
+++ Webcam_AForge_Edition/Form2.cs
@@ -27,63 +27,72 @@ namespace Webcam_AForge_Edition
 
         public Form2(System.Drawing.Image im)
         {
-            Imagefromform1 = im;
-
-            InitializeComponent();
-
-            pictureBoxForm2.Image = Imagefromform1; //Displays the picture in the picturebox
-
-            //Creation of greyscale filter
-            Grayscale filter = new Grayscale(0.2125, 0.7154, 0.0721);
-            Bitmap bt = new Bitmap(pictureBoxForm2.Image);
-
-            //applying the filter
-            Bitmap grayImage = filter.Apply(bt);
-            pictureBoxForm2.Image = (System.Drawing.Image)grayImage.Clone(); //clones the picture grayImage and displays it on the left
-
-            //-----------------------------------------------------------------------------------------------------------------------------------
-
-            // collect statistics
-            ImageStatistics his = new ImageStatistics(grayImage);
-            // get gray histogram (for grayscale image)
-            Histogram histogram = his.Gray;
-
-            // output some histogram's information
-            System.Diagnostics.Debug.WriteLine("Mean = " + histogram.Mean);
-            System.Diagnostics.Debug.WriteLine("Min = " + histogram.Min);
-            System.Diagnostics.Debug.WriteLine("Max = " + histogram.Max);
-
-
-            //-----------------------------------------------------------------------------------------------------------------------------------
-            // series1
-            //
-            var series1 = new Series
+            try
             {
-                Name = "series1",
-                // Disse og mange andre kan bruges, hvis man vil sætte properties i runtime (altså under programkørslen)
-                //LegendText = "Series LegendText",
-                //IsVisibleInLegend = true,
-                //IsXValueIndexed = true,
-                Color = Color.Black,
-                ChartType = SeriesChartType.Column
-            };
+                Imagefromform1 = im;
 
-            // Add datapoints to Series
-            for (int i = 0; i < histogram.Values.Count(); i++)
-            {
-                series1.Points.AddXY(xData[i], histogram.Values[i]);
+                InitializeComponent();
+
+                pictureBoxForm2.Image = Imagefromform1; //Displays the picture in the picturebox
+
+                //Creation of greyscale filter
+                Grayscale filter = new Grayscale(0.2125, 0.7154, 0.0721);
+                Bitmap bt = new Bitmap(pictureBoxForm2.Image);
+
+                //applying the filter
+                Bitmap grayImage = filter.Apply(bt);
+                pictureBoxForm2.Image = (System.Drawing.Image)grayImage.Clone(); //clones the picture grayImage and displays it on the left
+
+                //-----------------------------------------------------------------------------------------------------------------------------------
+
+                // collect statistics
+                ImageStatistics his = new ImageStatistics(grayImage);
+                // get gray histogram (for grayscale image)
+                Histogram histogram = his.Gray;
+
+                // output some histogram's information
+                System.Diagnostics.Debug.WriteLine("Mean = " + histogram.Mean);
+                System.Diagnostics.Debug.WriteLine("Min = " + histogram.Min);
+                System.Diagnostics.Debug.WriteLine("Max = " + histogram.Max);
+
+
+                //-----------------------------------------------------------------------------------------------------------------------------------
+                // series1
+                //
+                var series1 = new Series
+                {
+                    Name = "series1",
+                    // Disse og mange andre kan bruges, hvis man vil sætte properties i runtime (altså under programkørslen)
+                    //LegendText = "Series LegendText",
+                    //IsVisibleInLegend = true,
+                    //IsXValueIndexed = true,
+                    Color = Color.Black,
+                    ChartType = SeriesChartType.Column
+                };
+
+                // Add datapoints to Series
+                for (int i = 0; i < histogram.Values.Count(); i++)
+                {
+                    series1.Points.AddXY(xData[i], histogram.Values[i]);
+                }
+                // Transfer series data to form chart series
+                chart1.Series["Series1"] = series1;
+
+
+                //// Add datapoints to Series
+                //for (int i = 0; i < yData.Count(); i++)
+                //{
+                //    series1.Points.AddXY(histogram.Values, yData[i]);
+                //}
+                //// Transfer series data to form chart series
+                //chart1.Series["Series1"] = series1;
+
             }
-            // Transfer series data to form chart series
-            chart1.Series["Series1"] = series1;
+            catch (NullReferenceException)
+            {
+                MessageBox.Show("You need to capture a picture first");
+            }
 
-
-            //// Add datapoints to Series
-            //for (int i = 0; i < yData.Count(); i++)
-            //{
-            //    series1.Points.AddXY(histogram.Values, yData[i]);
-            //}
-            //// Transfer series data to form chart series
-            //chart1.Series["Series1"] = series1;
         }
     }
 }
